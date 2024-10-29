29b8141bcdbfcfa5ddc23f6ae23cdddd6d683597
brocolis é bom <134985029+n1lima@users.noreply.github.com>
2024-10-29 03:29:40 -0300

    removendo arquivos

--- somadorCompleto.v
+++
@@ -1,9 +0,0 @@
-module somadorCompleto(a, b, c_in, s, c_out);
-
-    input a, b, c_in;
-    output s, c_out;
-
-    assign s = a ^ b ^ c_in;
-    assign c_out = b&c_in | a&c_in | a&b;
-
-endmodule
--- subtrator.v
+++
@@ -1,9 +0,0 @@
-module subtrator(a,b,c,s);
-
-    input a,b;
-    output c,s;
-
-    assign s = a ^ b;
-    assign c = (~a)& b;
-    
-endmodule
